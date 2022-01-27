746d60fc1bf7eb6240726393e6a26bec4ec062f0
Asad Ur Rahman <AsadRahman511@gmail.com>
2022-01-27 22:34:38 +0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,10 +1,10 @@
 import express from 'express'
-//import cors from 'cors'
+import cors from 'cors'
 
 
 const app = express()
 
-//app.use(cors())
+app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
