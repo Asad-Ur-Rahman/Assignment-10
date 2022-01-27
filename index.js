00a81314ea34f357a3fc8d6ace04a8213d9d272b
Asad Ur Rahman <AsadRahman511@gmail.com>
2022-01-27 23:07:35 +0500

    html file added

---
+++ index.js
@@ -0,0 +1,39 @@
+function getData() {
+
+    let opt = document.getElementById("opt")
+    opt.innerText = '';
+
+    axios.get('https://the-server-1.herokuapp.com/posts')
+        .then(function (response) {
+            // handle success
+            // console.log(response.data);
+            // opt.innerText = response.data;
+            for (const key in response.data) {
+                //console.log(response.data[key]['text']);
+
+                opt.innerText +=`${response.data[key]['text']} \n`
+                
+            }
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
+}
+
+
+function postData(){
+
+    const val = document.getElementById("txt").value;
+
+    axios.post('https://the-server-1.herokuapp.com/post', {
+        "text": val
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+}
